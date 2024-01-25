8914c10b1758c8aa7d5105016870b8c4fb2ffc76
Vinay Kumar <156786126+TheExtremezCoder@users.noreply.github.com>
2024-01-25 11:20:15 +0000

    hey

--- src/config.js
+++ src/config.js
@@ -1,39 +1,39 @@
 require("dotenv").config();
 
 module.exports = {
-    token: process.env.TOKEN || "",  // your bot token
-    prefix: process.env.PREFIX || ".", // bot prefix
-    ownerID: process.env.OWNERID ||  ["1173547185758015498"], //your discord id
-    mongourl: process.env.MONGO_URI || "mongodb+srv://pee33:wnn99e@cluster0.fgivnid.mongodb.net/?retryWrites=true&w=majority", // MongoDb URL
-    topggapi : "",
-    embedColor: process.env.COlOR || "#00fff5", // embed colour
-    logs: process.env.LOGS || "1198586029343518820", // channel id for guild create and delete logs
-    langs:  process.env.LANGS || "en",
-    
+  token:
+    process.env.TOKEN ||
+    "MTE5NzU4NTg4MjgwMzM0MzM5MQ.GTKYZ-.5YZaQKUSTKp4GLdgCXRgWlmCbRbYOLlFSOmh-g", // your bot token
+  prefix: process.env.PREFIX || ".", // bot prefix
+  ownerID: process.env.OWNERID || ["1173547185758015498"], //your discord id
+  mongourl:
+    process.env.MONGO_URI ||
+    "mongodb+srv://pee33:wnn99e@cluster0.fgivnid.mongodb.net/?retryWrites=true&w=majority", // MongoDb URL
+  topggapi: "",
+  embedColor: process.env.COlOR || "#00fff5", // embed colour
+  logs: process.env.LOGS || "1198586029343518820", // channel id for guild create and delete logs
+  langs: process.env.LANGS || "en",
 
-    nodes: [
+  nodes: [
     {
-      host: process.env.NODE_HOST ||"lava.link",
+      host: process.env.NODE_HOST || "lava.link",
       identifer: process.env.NODE_ID || "Main",
       port: parseInt(process.env.NODE_PORT || "80"),
       password: process.env.NODE_PASSWORD || "LAVA",
       secure: parseBoolean(process.env.NODE_SECURE || "false"),
-
-    }
+    },
   ],
+};
 
-
-}
-
-function parseBoolean(value){
-    if (typeof(value) === 'string'){
-        value = value.trim().toLowerCase();
-    }
-    switch(value){
-        case true:
-        case "true":
-            return true;
-        default:
-            return false;
-    }
+function parseBoolean(value) {
+  if (typeof value === "string") {
+    value = value.trim().toLowerCase();
+  }
+  switch (value) {
+    case true:
+    case "true":
+      return true;
+    default:
+      return false;
+  }
 }
